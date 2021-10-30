cd616ee294129583b31010b180c424432fb2a78d
Simeet Nayan <simeetnayan.8100@gmail.com>
2021-10-30 17:03:13 +0530

    initial commit

---
+++ predict.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import numpy as np
+import pickle
+# text preprocessing
+import nltk
+import re
+from keras.models import load_model
+# plots and metrics
+import matplotlib.pyplot as plt
+from sklearn.metrics import accuracy_score, f1_score, confusion_matrix
+# preparing input to our model
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/pred/<string:message>")
+
+def pred(message):
+    class_names = ['joy', 'fear', 'anger', 'sadness', 'neutral']
+    tokenizer=None
+    with open('tokenizer.pickle', 'rb') as handle:
+        tokenizer = pickle.load(handle)
+
+    model = load_model('cnn_w2v.h5')
+    seq = tokenizer.texts_to_sequences([message])
+    padded = pad_sequences(seq, maxlen=500)
+    pred = model.predict(padded)
+    result= {
+        "message": message,
+        "emotion": class_names[np.argmax(pred)]
+        }
+    return result
+
+
+print(pred('I am very happy today'))
+
+if __name__=="__main__":
+    app.run(debug=True)
