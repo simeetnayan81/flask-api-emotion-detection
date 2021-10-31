522889fc365137628427f1c82ff791254404d07c
Simeet Nayan <simeetnayan.8100@gmail.com>
2021-10-31 12:51:30 +0530

    updated

--- app.py
+++ app.py
@@ -5,46 +5,17 @@ from nltk import word_tokenize
 from nltk.stem import PorterStemmer
 from nltk.corpus import stopwords
 import re
-
-
+import pretoken
 
 app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def pred():
-    def preprocess_and_tokenize(data):
-        data = re.sub("(<.*?>)", "", data)
-
-    #remove urls
-        data = re.sub(r'http\S+', '', data)
-
-    #remove hashtags and @names
-        data= re.sub(r"(#[\d\w\.]+)", '', data)
-        data= re.sub(r"(@[\d\w\.]+)", '', data)
-
-    #remove punctuation and non-ascii digits
-        data = re.sub("(\\W|\\d)", " ", data)
-
-    #remove whitespace
-        data = data.strip()
-
-    # tokenization with nltk
-        data = word_tokenize(data)
-
-    # stemming with nltk
-        porter = PorterStemmer()
-        stem_data = [porter.stem(word) for word in data]
-
-        return stem_data
     message= request.args.get("message", None)
-    filename = 'tfidf_svm.sav'
-    model = pickle.load(open(filename, 'rb'))
-    #message = 'delivery was hour late and my pizza is cold!'
-    data=[message]
-    pred = model.predict(data)
+    emo=pretoken.mymod(message)
     result= {
         "message": message,
-        "emotion": pred[0]
+        "emotion": emo[0]
         }
     with app.app_context():
         jsonify(result)
@@ -53,30 +24,6 @@ def pred():
 
 
 
-
 if __name__=="__main__":
-    def preprocess_and_tokenize(data):
-        data = re.sub("(<.*?>)", "", data)
-
-    #remove urls
-        data = re.sub(r'http\S+', '', data)
-
-    #remove hashtags and @names
-        data= re.sub(r"(#[\d\w\.]+)", '', data)
-        data= re.sub(r"(@[\d\w\.]+)", '', data)
-
-    #remove punctuation and non-ascii digits
-        data = re.sub("(\\W|\\d)", " ", data)
-
-    #remove whitespace
-        data = data.strip()
-
-    # tokenization with nltk
-        data = word_tokenize(data)
-
-    # stemming with nltk
-        porter = PorterStemmer()
-        stem_data = [porter.stem(word) for word in data]
-
-        return stem_data
+    from pretoken import preprocess_and_tokenize, mymod
     app.run()
---
+++ pretoken.py
@@ -0,0 +1,37 @@
+from nltk import word_tokenize
+from nltk.stem import PorterStemmer
+from nltk.corpus import stopwords
+import re
+import pickle
+
+def preprocess_and_tokenize(data):
+    data = re.sub("(<.*?>)", "", data)
+
+    #remove urls
+    data = re.sub(r'http\S+', '', data)
+
+    #remove hashtags and @names
+    data= re.sub(r"(#[\d\w\.]+)", '', data)
+    data= re.sub(r"(@[\d\w\.]+)", '', data)
+
+    #remove punctuation and non-ascii digits
+    data = re.sub("(\\W|\\d)", " ", data)
+
+    #remove whitespace
+    data = data.strip()
+
+    # tokenization with nltk
+    data = word_tokenize(data)
+
+    # stemming with nltk
+    porter = PorterStemmer()
+    stem_data = [porter.stem(word) for word in data]
+
+    return stem_data
+def mymod(message):
+    filename = 'tfidf_svm.sav'
+    model = pickle.load(open(filename, 'rb'))
+    #message = 'delivery was hour late and my pizza is cold!'
+    data=[message]
+    pred = model.predict(data)
+    return pred
