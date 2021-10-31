0d09b617e86c3d9ea0c78a89fa33917ad6c4dbc2
Simeet Nayan <simeetnayan.8100@gmail.com>
2021-10-31 10:36:01 +0530

    updated

--- app.py
+++ app.py
@@ -4,19 +4,12 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/")
 def pred():
-    message= request.args.get("message", None)
-    result= {
-        "message": message,
-        "emotion": "Will do it too"
-        }
-    with app.app_context():
-        jsonify(result)
-    return result
+    return 'Hello World'
 
 
 
 
 if __name__=="__main__":
-    app.run(threaded=True, port=5000)
+    app.run()
